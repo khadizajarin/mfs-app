001727ab127e133daa669ad13962fafff87fdb5b
khadizajarin <khadiza131310@gmail.com>
2025-02-27 02:17:14 +0600

    api update

--- client/src/app/profile/page.js
+++ client/src/app/profile/page.js
@@ -24,7 +24,7 @@ export default function ProfilePage() {
   
     const fetchUserData = async () => {
       try {
-        const response = await axios.get(`https://mfs-hw0a5h057-khadizajarins-projects.vercel.app/api/users`, {
+        const response = await axios.get(`https://mfs-3b9bdidim-khadizajarins-projects.vercel.app/api/users`, {
           params: { email: user.email }
         });
   
